385d11622de24801e802292f6809326160e8ae0e
Snickers <sp3sh.me@gmail.com>
2023-02-05 11:18:37 +1000

    Update SnicksLure.lua

--- SnicksLure/SnicksLure.lua
+++ SnicksLure/SnicksLure.lua
@@ -1,9 +1,4 @@
 print("Lures Applicator by Snickers (published by Moose)")
-local function IsFishingRodEnchanted()
-  local HasMainHandEnchant, _, _, _ = GetWeaponEnchantInfo()
-
-  return HasMainHandEnchant
-end
 
 local openitem = {
     [7973] = "Big-mouth Clam",
@@ -18,22 +13,23 @@ local Frame = CreateFrame('Frame')
 Frame:SetScript("OnUpdate", function(self, elapsed)
     if GetTime() > NextCheck then
         NextCheck = GetTime() + CheckInterval
-        if __LB__ ~= nil then
-            if not IsFishingRodEnchanted() then
-                __LB__.Unlock(RunMacro, "Fishing")
-                ShouldRestart = true
-                print("Applying lure, fishing will restart in 10 seconds")
-            elseif ShouldRestart then
+
+        if IsFishingRodEnchanted() then
+            for id, name in pairs(openitem) do
+                if GetItemCount(id) > 0 then
+                    UseItemByName(name)
+                end
+            end
+        else
+            if ShouldRestart then
                 __LB__.Unlock(RunMacro, "Fishing2")
                 ShouldRestart = false
                 print("Fishing restarted")
-                else
-                    for Id, Name in pairs(openitem) do
-                        if GetItemCount(Id) > 0 then
-                            UseItemByName(Id)
-                        end
-                    end
-                end
+            else
+                __LB__.Unlock(RunMacro, "Fishing")
+                ShouldRestart = true
+                print("Applying lure, fishing will restart in 10 seconds")
             end
         end
+    end
 end)
